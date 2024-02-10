dff0506a87244ddd6b8c99caff9225fbf1cb716a
kishanshettyb <kishanqr@gmail.com>
2024-02-10 12:36:30 +0530

    fix issues

--- src/sections/Workshop.js
+++ src/sections/Workshop.js
@@ -27,14 +27,16 @@ const Workshop = () => {
 				breakpoint: 600,
 				settings: {
 					slidesToShow: 2,
-					slidesToScroll: 2
+					slidesToScroll: 2,
+					dots: false
 				}
 			},
 			{
 				breakpoint: 480,
 				settings: {
 					slidesToShow: 1,
-					slidesToScroll: 1
+					slidesToScroll: 1,
+					dots: false
 				}
 			}
 		]

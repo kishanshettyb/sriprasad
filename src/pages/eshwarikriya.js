1a44254690c395a48d8b3f0d937efd7bde0d2d56
kishanshettyb <kishanqr@gmail.com>
2024-02-12 13:45:26 +0530

    add eshwari kriya page

---
+++ src/pages/eshwarikriya.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Layout from "../components/Layout";
+import PageBanner from "../components/PageBanner";
+
+const eshwarikriya = () => {
+	return (
+		<Layout>
+			<Container fluid>
+				<PageBanner />
+			</Container>
+			<Container>
+				<Row>
+					<Col></Col>
+				</Row>
+			</Container>
+		</Layout>
+	);
+};
+
+export default eshwarikriya;

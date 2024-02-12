4f73928eb887352a1fce2901e30d2f6c87fba150
kishanshettyb <kishanqr@gmail.com>
2024-02-12 16:35:57 +0530

    add new page

---
+++ src/pages/srikari.js
@@ -0,0 +1,152 @@
+import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Layout from "../components/Layout";
+import PageBanner from "../components/PageBanner";
+import { StaticImage } from "gatsby-plugin-image";
+
+const srikari = () => {
+	return (
+		<Layout>
+			<PageBanner title="Sri Kari" subTitle="A time testified, traditional approach to manifesting intentions" />
+			<section>
+				<Container>
+					<h2 className="w-75 m-auto display-2 fw-bolder text-center mb-2"> SriKari </h2>
+					<p className="text-center w-75 m-auto fw-bold fs-5 opacity-75 mb-5">
+						Srikari : Kari means giver of boons and Sri meaning the Devi who bestows all; health, protection, prosperity, happiness and success.​
+					</p>
+					<Row className="align-items-center">
+						<Col md={6}>
+							<h2 className="display-5 fw-bold mb-4">How SriKari was started?</h2>
+							<p>
+								Started in the form of Srikari worship, was initiated by guruji in 2009, earlier Srikari was performed as an external ritual worship at individual houses in
+								gatherings involving all members of a particular household followed up by bhajans and dance.
+							</p>
+							<p>
+								<b>Srikari</b> had once become a festivity through the word of mouth which occupied the hearts of families and was regularly performed at times when there were
+								happy moments such as birth of a child, birthday’s, success in business ventures etc and Srikari was also performed for uplifting lives of householders who were
+								bearing through bad times due to losses.
+							</p>
+							<p>
+								<b>Srikari</b>was welcome as any other festival that had paved way into the lives of thousands of volunteers and devotees along with their family members to welcome
+								abundance into their lives.
+							</p>
+
+							<p>
+								Over the years from its inception, Srikari had become so popular that performing it for individual families had become too tedious to handle and then guruji worked
+								a way for bringing Srikari to everyone in the year 2016 for those who desire to undergo it, by bringing them all together to one place once during the year to
+								participate , perform and do sankalpa on their own through meditation and other methods in a celebrative environment and still reap full benefits just as is
+								begotten from an external worship.
+							</p>
+						</Col>
+						<Col md={6} className="text-center">
+							<StaticImage className="img-fluid br-20 mb-4" src="../images/banner/srikari.jpeg" alt="Page Banner" />
+							<Button variant="primary" size="lg" className="px-5 mb-4">
+								Register Now
+							</Button>
+						</Col>
+						<Col md={12}>
+							<hr />
+							<h2 className="display-5 fw-bold my-4 ">A time testified, traditional approach to manifesting intentions</h2>
+							<p>
+								An idea based on the science of what you intend is what you manifest, to explain this in simple it is what you think, feel and dream or intend subconsciously or
+								consciously eventually manifests also meaning to say that the predominant thoughts and feelings is what manifests into reality. The method followed during the event
+								is yogic based, an authentic time tested method of manifesting intentions also known in Sanskrit as Sankalpa Siddhi.
+							</p>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+
+			<section className="bg-light-1">
+				<Container>
+					<Row>
+						<Col md={6}>
+							<h2 className="display-5 fw-bold mb-4">The yogic process one undergoes at the event is: </h2>
+							<h2 className="display-6 fw-bold mb-5 text-primary">Srichakra Sankalpa Siddhi puja</h2>
+							<p>A shakta(feminine worhipper) tantric way of worshipping cosmic mother in the geometric form called the Srichakra known popularly as the king of all chakras.</p>
+							<p>
+								Srichakra includes salutations to Sri Guru Parampara, Sri Ganesha, Sun with 8 planets, Shiva and Mahavishnu along with 108 deities, 64 yoginis along with 64
+								bhairavas in the Sankalpa Siddhi puja, with the whole puja process taking upto 45min to perform and offer puja tarpanam, the main deity being worshiped Devi
+								Tripureshwari.
+							</p>
+							<p>in the presence of Sri Guru Sri Prasad (Sri Prasadanandha Natha Yogi )</p>
+							<p>
+								The sankalpa siddhi puja nullifies karmic impressions, turns malefic planetary influences to beneficiaries, provides relief from diseases, destroys sadness and
+								sorrow.
+							</p>
+						</Col>
+						<Col md={6}>
+							<StaticImage className="img-fluid br-20" src="../images/banner/pooja.jpeg" alt="Page Banner" />
+						</Col>
+					</Row>
+				</Container>
+			</section>
+
+			<section>
+				<Container>
+					<Row className="align-items-center">
+						<Col md={6}>
+							<h2 className="display-5 fw-bold mb-4">Sankalpa Siddhi meditation and Deeper karmic cleansing process</h2>
+							<p>
+								For fulfillment of all things in life there’s only one thing that can open all the doors to possibility and for that yoga specifies meditation and meditation alone
+								which is the well known subjective journey within, Sankalpa Siddhi meditation is one such guided journey to manifest your intentions.
+							</p>
+
+							<p>
+								Transcending the states of wakefulness, dream and deep sleep states of Consciousness and diving into the fourth state of consciousness which is also known as the
+								transcendental consciousness is the state of all possibilities.{" "}
+							</p>
+
+							<p>
+								When you progress in meditation, unfolding the 4th state of consciousness with complete awareness and sow the seed of your intention in that state of consciousness
+								called sankalpa, is known to be difficult of practices, when attained germinates and grows into a serene tree.
+							</p>
+
+							<p>
+								This process of taking someone there and especially an audience so big is a mind bending task, you are guided and driven into that state of consciousness carrying
+								your intentions which will be sown by you in your own transcendental state of mind and made manifest by guruji at the event in the fullness of joy, dance, happiness
+								and celebrative environment.
+							</p>
+						</Col>
+						<Col md={6}>
+							<StaticImage className="img-fluid br-20" src="../images/banner/sankalpa.jpeg" alt="Page Banner" />
+						</Col>
+					</Row>
+				</Container>
+			</section>
+
+			<section className="bg-light-1">
+				<Container>
+					<Row className="align-items-center">
+						<Col md={6}>
+							<StaticImage className="img-fluid br-20" src="../images/banner/bless.jpeg" alt="Page Banner" />
+						</Col>
+						<Col md={6}>
+							<h2 className="display-5 fw-bold mb-4">Authentic scripture based method of manifestation:</h2>
+
+							<p>
+								The manifestation happens due to a bona-fide communication with the universal consciousness i.e. the cosmic mother Tripureshwari, there are four types of
+								communication which is mentioned in the yogic science and vedas, the same is found mentioned in the Lalita Sahasranamam verse 81 that reads{" "}
+							</p>
+
+							<h2 className="display-5 fw-bold mb-4 special-title">||Parapratyak chitirupa pashyanti paradevata Madhyama vaikharirupa bhakta manasa hamsika|| LS 81</h2>
+
+							<p>
+								Meaning the communication with the divine is possible in 4 ways , first is by speech , second is through feelings, third is through visualization and the fourth is
+								a very powerful way of communication which is PARA meaning that which happens in the transcendental state of consciousness, the four ways are named Vikhari meaning
+								speech, Madhyama meaning feelings, Pashyanti meaning visualization and Para which literally means transcendental state of consciousness.
+							</p>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+			<section className="bg-gray-light-1">
+				<Container>
+					<h2 className="w-75 m-auto display-2 fw-bolder text-center mb-2"></h2>
+				</Container>
+			</section>
+		</Layout>
+	);
+};
+
+export default srikari;
